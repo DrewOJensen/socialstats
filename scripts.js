c8dec07c981ccb5a81a9c21cd417343f000f8848
Drew Jensen <jens0771@gmail.com>
2017-09-03 08:03:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-var teamList = { minnesotaunited: "Minnesota United FC", SoundersFC: "Seattle Sounders FC", timbers: "Portland Timbers", NYCFC: "New York City FC", sportingkc: "Sporting Kansas City",PhillyUnion: "Philadelphia Union",TFC: "Toronto FC",OCLions: "Orlando City SC",rbny: "New York Red Bulls",LAGalaxy: "LA Galaxy",DCUnited: "D.C. United",TheMassive: "Columbus Crew SC",SJEarthquakes: "San Jose Earthquakes",Chicagofire: "Chicago Fire",NewEnglandRevolution: "New England Revolution",dynamo: "Houston Dynamo",whitecapsfc: "Vancouver Whitecaps FC",FCDallas: "FC Dallas",realsaltlake: "Real Salt Lake",Rapids: "Colorado Rapids",montrealimpact: "Montreal Impact", AtlantaUnited: "Atlanta United FC"}
+var teamList = { minnesotaunited: "Minnesota United FC", SoundersFC: "Seattle Sounders FC", timbers: "Portland Timbers", NYCFC: "New York City FC", sportingkc: "Sporting Kansas City",PhillyUnion: "Philadelphia Union",TFC: "Toronto FC",OCLions: "Orlando City SC",rbny: "New York Red Bulls",LAGalaxy: "LA Galaxy",DCUnited: "D.C. United",TheMassive: "Columbus Crew SC",SJEarthquakes: "San Jose Earthquakes",Chicagofire: "Chicago Fire",NewEnglandRevolution: "New England Revolution",dynamo: "Houston Dynamo",whitecapsfc: "Vancouver Whitecaps FC",FCDallas: "FC Dallas",realsaltlake: "Real Salt Lake",Rapids: "Colorado Rapids",montrealimpact: "Montreal Impact",AtlantaUnited: "Atlanta United FC"}
+
+var sortable = [];
 
 $.each( teamList, function( key, val ) {
-  $.getJSON(
-    "https://api.reddit.com/r/"+key+"/about",
-    function foo(data)
-    {
-      $("#team").append(val + " : ");
-      $("#team").append(data.data.subscribers + '<br>');
+  $.ajax({
+    url: "https://api.reddit.com/r/"+key+"/about",
+    async: false,
+    dataType: 'json',
+    success: function(data) {
+      subNum = data.data.subscribers;
+      sortable.push([val,subNum])
+      //$("#team").append(val + " : "); //DEBUG
+      //$("#team").append( subNum + '<br>');  //DEBUG
     }
-  );
-});
+  });
+})
+
+sortable.sort(function(a, b){
+    return b[1]-a[1]
+})
+
+//console.log(sortable) //DEBUG
+
+for (var i=0,  tot=sortable.length; i < tot; i++) {
+  $("#team").append(sortable[i][0] + " : ");
+  $("#team").append(sortable[i][1] + '<br>');
+}
